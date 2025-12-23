203009e9f7641aa0fea1d6c6a658b6ba9d0eab26
Raushan Kumar Thakur <rnkp755@gmail.com>
2025-12-23 16:05:17 +0530

    Never use Devise+JWT in an API only project.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/current_user", to: "current_user#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,7 +8,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
+  devise_for :users, path: "", path_names: {
+    sign_in: "login",
+    sign_out: "logout",
+    registration: "signup"
+  },
+  controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   namespace :api do
     namespace :v1 do
       resources :challenges
